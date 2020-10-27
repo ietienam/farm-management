4f7a93301b63990206e51df68b3cccfdbe137a85
Ini Etienam <ietienam@gmail.com>
2020-10-27 21:04:20 +0100

    auth refactored

--- controller/authController.js
+++ controller/authController.js
@@ -3,7 +3,7 @@
 var { promisify } = require("util"); //CONVERT FUNCTION TO ASYNC TO AWAIT A PROMISE
 var jwt = require("jsonwebtoken");
 var AppError = require("../utils/appError");
-var catchAsync = require('../utils/catchAsync');
+var catchAsync = require("../utils/catchAsync");
 
 exports.protect = catchAsync(async (req, res, next) => {
   //GETTING TOKEN AND CHECK IF ITS THERE
@@ -11,19 +11,21 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   if (req.headers.authorization && req.headers.authorization !== "") {
     token = req.headers.authorization;
-  }
 
-  if (!token) {
-    return next(new AppError("Access denied! Please logIn", 401));
-  } else {
-    //VERIFY TOKEN
-    const decoded = await promisify(jwt.verify)(token, 'mysecret');
-    console.log(decoded.id);
-
-    if (decoded.id) {
-      next();
+    if (!token) {
+      return next(new AppError("Access denied! Please logIn", 401));
     } else {
-      return next(new AppError('Token not valid. Please login!', 400));
+      //VERIFY TOKEN
+      const decoded = await promisify(jwt.verify)(token, "mysecret");
+      console.log(decoded.id);
+
+      if (decoded.id) {
+        next();
+      } else {
+        return next(new AppError("Token not valid. Please login!", 400));
+      }
     }
+  } else {
+    return next(new AppError("Access denied! Please logIn", 401));
   }
 });
